0c71b14b0c80b327164ba61b0202a7a5c74f6920
Alexis Foerster <alexis.foerster@gmail.com>
2019-02-02 14:56:14 +0100

    Implémentation des éléments Tools (non terminés).

--- EditionMetadonnees.pro
+++ EditionMetadonnees.pro
@@ -18,13 +18,15 @@ HEADERS += include/Data.h \
            include/DocumentationWindow.h \
            include/MainWindow.h \
            include/MP3File.h \
-           include/Settings.h
+           include/Settings.h \
+           include/Tools.h
 SOURCES += source/EditionMetadonnees.cpp \
            source/Data.cpp \
            source/DocumentationWindow.cpp \
            source/MainWindow.cpp \
            source/MP3File.cpp \
-           source/Settings.cpp
+           source/Settings.cpp \
+           source/Tools.cpp
 FORMS += interfaces/DocumentationWindow.ui \
          interfaces/MainWindow.ui
 RESOURCES += ressources/Ressources.qrc
---
+++ source/Tools.cpp
@@ -0,0 +1,30 @@
+//==============================================================================
+// Name        : Tools.cpp
+// Author      : Alexis Foerster (alexis.foerster@gmail.com)
+// Version     : 1.0.0 (01/02/2019)
+// Description : Source file of the Tools elements
+//==============================================================================
+
+#include "Tools.h"
+
+int executeCommand(const QString& command, const bool& log)
+{
+    // TODO int executeCommand(const QString& command, const bool& log)
+    Q_UNUSED(command);
+    Q_UNUSED(log);
+    return 0;
+}
+
+const QStringList readFileLines(const QString& filePath)
+{
+    // TODO const QStringList readFileLines(const QString& filePath)
+    Q_UNUSED(filePath);
+    return QStringList();
+}
+
+void writeFileLines(const QString& filePath, const QStringList& lines)
+{
+    // TODO void writeFileLines(const QString& filePath, const QStringList& lines)
+    Q_UNUSED(filePath);
+    Q_UNUSED(lines);
+}
